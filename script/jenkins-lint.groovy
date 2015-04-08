c970654b157328f77a3b859b07c816623fcaf52f
Victor Martinez <VictorMartinezRubio@gmail.com>
2015-04-08 20:50:52 +0200

    Added groovydoc and ignored validation

--- script/jenkins-lint.groovy
+++ script/jenkins-lint.groovy
@@ -19,7 +19,7 @@ def jobs = Hudson.instance?.items
 def RULE_JOB_NAME    = "[JL-000] Job name"
 def RULE_LOG_ROTATOR = "[JL-001] Log Rotator doesn't exist"
 def RULE_DESCRITION  = "[JL-002] Description hasn't been set"
-def RULE_SCM	     = "[JL-003] SCM hasn't been set"
+def RULE_SCM	       = "[JL-003] SCM hasn't been set"
 def RULE_SCM_TRIGGER = "[JL-004] SCM trigger is polling rather than pulling"
 def RULE_SCM_DUPLICATED_TRIGGER = "[JL-005] SCM trigger is duplicated"
 def RULE_LABELS 	 = "[JL-006] Restric executions"
@@ -31,38 +31,85 @@ def RULE_MVN_JOB_TYPE = "[JL-011] Maven Job Type" //http://www.slideshare.net/an
 def RULE_SCM_GIT_SHALLOW = "[JL-012] Git shallow"
 
 /**
- * provided script parameter or default value
+ * Print rule with provided rule and jobName.
  *
- * @return paramTag - tag filter pattern
+ * @param ruleDescription Text to be included in the header.
+ * @param jobName Name of the Job.
  */
-def printRule(description, job) {
-   println "\t$description $job"
+def printRule(rule, jobName) {
+   println "\t$rule $jobName"
 }
 
+/**
+ * Return rule id.
+ *
+ * @param ruleDescription Rule description.
+ * @return String indicating ruleId.
+ */
+def getRuleId(ruleDescription){
+    myRegularExpression = /\[(JL-\d+)\] .+/
+    matcher = ( ruleDescription =~ myRegularExpression )
+
+    if (matcher.matches()) {
+      matcher[0][1]
+    }
+}
+
+/**
+ * Return if ruleId has been ignored in the job description.
+ *
+ * @param ruleId Rule id.
+ * @param jobDescription Job description.
+ * @return Boolean whether that ruleId has been ignored or not.
+ */
+def isIgnored(ruleId, jobDescription){
+  if (ruleId != null && jobDescription != null) {
+    if (jobDescription.indexOf("lint:ignored:$ruleId") > -1) {
+      return true
+    }
+    return false
+  }
+  return false
+}
+
+/**
+ * Return if ruleId has been ignored in the job description.
+ *
+ * @param ruleDescription Rule description.
+ * @param jobName Job name.
+ * @param jobDescription Job description.
+ * @return Boolean whether that ruleId has been ignored or not.
+ */
+def runRule(ruleDescription, jobName, jobDescription) {
+  if (jobName != null && ruleDescription != null) {
+    if (!isIgnored(getRuleId(ruleDescription),jobDescription)){
+      printRule (ruleDescription, jobName)
+    }
+  }
+}
 
 // RULES
-
 jobs?.findAll{ !it.logRotator && !it.disabled }.each {
-	printRule (RULE_LOG_ROTATOR, it.name)
+	runRule (RULE_LOG_ROTATOR, it.name, it.description)
 }
 
 jobs?.findAll{ !it.description && !it.disabled }.each {
-  	printRule (RULE_DESCRITION, it.name)
+  runRule (RULE_DESCRITION, it.name, it.description)
 }
 
 jobs?.findAll{ it.scm instanceof hudson.scm.NullSCM && !it.disabled }.each {
-    printRule (RULE_SCM, it.name)
+  runRule (RULE_SCM, it.name, it.description)
 }
 
 jobs?.findAll{ !it.disabled && it.name.contains(" ") }.each {
-  	printRule (RULE_JOB_NAME, it.name)
+  	runRule (RULE_JOB_NAME, it.name, it.description)
 }
 
 TriggerDescriptor SCM_TRIGGER_DESCRIPTOR = Hudson.instance.getDescriptorOrDie(SCMTrigger.class)
 assert SCM_TRIGGER_DESCRIPTOR != null;
 
 jobs?.findAll{ it.triggers.get(SCM_TRIGGER_DESCRIPTOR) && it.triggers.get(SCM_TRIGGER_DESCRIPTOR) instanceof SCMTrigger && !it.disabled }.each {
-  	printRule (RULE_SCM_TRIGGER, it.name)
+  	runRule (RULE_SCM_TRIGGER, it.name, it.description)
 }
 
 TriggerDescriptor SCM_TIMER_TRIGGER_DESCRIPTOR = Hudson.instance.getDescriptorOrDie(TimerTrigger.class)
@@ -71,11 +118,11 @@ assert SCM_TIMER_TRIGGER_DESCRIPTOR != null;
 jobs?.findAll{ it.triggers.get(SCM_TIMER_TRIGGER_DESCRIPTOR) && it.triggers.get(SCM_TIMER_TRIGGER_DESCRIPTOR) instanceof TimerTrigger && 
                it.triggers.get(SCM_TRIGGER_DESCRIPTOR) && it.triggers.get(SCM_TRIGGER_DESCRIPTOR) instanceof SCMTrigger && 
                !it.disabled }.each {
-    printRule (RULE_SCM_DUPLICATED_TRIGGER, it.name)
+    runRule (RULE_SCM_DUPLICATED_TRIGGER, it.name, it.description)
 }
 
 jobs?.findAll{ !it.disabled && !it.getAssignedLabelString() }.each {
-    printRule (RULE_LABELS, it.name)
+    runRule (RULE_LABELS, it.name, it.description)
 }
 
 // PUBLISHERS
@@ -100,29 +147,29 @@ jobs?.findAll{ !it.disabled }.each {
         }
       }
   }
-  if (!hasCleanup) {     printRule (RULE_CLEANUP, it.name) }
-  if (!hasJavadoc) {     printRule (RULE_JAVADOC, it.name) }
-  if (!hasArtifact) {    printRule (RULE_ARTIFACT, it.name) }
+  if (!hasCleanup) {     runRule (RULE_CLEANUP, it.name, it.description) }
+  if (!hasJavadoc) {     runRule (RULE_JAVADOC, it.name, it.description) }
+  if (!hasArtifact) {    runRule (RULE_ARTIFACT, it.name, it.description) }
 }
 
 // BUILDERS
 jobs?.findAll{ !it.disabled && !it instanceof hudson.maven.MavenModuleSet && it.builders}.each {
   for(p in it.builders) {
     if (( p instanceof hudson.tasks.Shell || p instanceof hudson.tasks.BatchFile ) && p.getContents().split("\r\n|\r|\n").length > 3) {
-      printRule (RULE_HARCODED_SCRIPT, it.name)
+      runRule (RULE_HARCODED_SCRIPT, it.name, it.description)
     }
   }
 }
 
 jobs?.findAll{ !it.disabled && it instanceof hudson.maven.MavenModuleSet}.each {
-  printRule (RULE_MVN_JOB_TYPE, it.name)
+  runRule (RULE_MVN_JOB_TYPE, it.name, it.description)
 }
 
 // GIT SCM
 jobs?.findAll{ it.scm && it.scm instanceof hudson.plugins.git.GitSCM && it.scm.getExtensions() && !it.disabled }.each {
   for (p in it.scm.getExtensions()) {
     if (p instanceof hudson.plugins.git.extensions.impl.CloneOption && !p.isShallow()){
-  		printRule (RULE_SCM_GIT_SHALLOW, it.name)
+  		runRule (RULE_SCM_GIT_SHALLOW, it.name, it.description)
     }
   }
 }
