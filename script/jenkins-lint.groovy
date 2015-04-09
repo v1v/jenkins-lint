e2b970100d959cb5b4bdb4c6c671fba998035969
Victor Martinez <VictorMartinezRubio@gmail.com>
2015-04-09 21:50:15 +0200

    Added multijob rule and removed scope variables

--- script/jenkins-lint.groovy
+++ script/jenkins-lint.groovy
@@ -1,34 +1,24 @@
 import hudson.model.*
 import hudson.triggers.*
 
-//https://github.com/JLci/JL-scripts/blob/master/scriptler/barchartGitTagList.groovy
-//https://scriptlerweb.appspot.com/catalog/list
-  
-/**
-import net.sf.json.*
-
-@Grapes([
-    @Grab('org.kohsuke.stapler:json-lib:2.4-JL-1')
-])
-*/
-  
 // list of jobs
-def jobs = Hudson.instance?.items
+jobs = Hudson.instance?.items
 
 // RULES
-def RULE_JOB_NAME    = "[JL-000] Job name"
-def RULE_LOG_ROTATOR = "[JL-001] Log Rotator doesn't exist"
-def RULE_DESCRITION  = "[JL-002] Description hasn't been set"
-def RULE_SCM	       = "[JL-003] SCM hasn't been set"
-def RULE_SCM_TRIGGER = "[JL-004] SCM trigger is polling rather than pulling"
-def RULE_SCM_DUPLICATED_TRIGGER = "[JL-005] SCM trigger is duplicated"
-def RULE_LABELS 	 = "[JL-006] Restric executions"
-def RULE_CLEANUP 	 = "[JL-007] CleanUp Workspace"
-def RULE_JAVADOC 	 = "[JL-008] Javadoc"
-def RULE_ARTIFACT 	 = "[JL-009] Artifact archiver"
-def RULE_HARCODED_SCRIPT = "[JL-010] Harcoded Script over 3 lines"
-def RULE_MVN_JOB_TYPE = "[JL-011] Maven Job Type" //http://www.slideshare.net/andrewbayer/seven-habits-of-highly-effective-JL-users-2014-edition
-def RULE_SCM_GIT_SHALLOW = "[JL-012] Git shallow"
+RULE_JOB_NAME    = "[JL-000] Job name"
+RULE_LOG_ROTATOR = "[JL-001] Log Rotator doesn't exist"
+RULE_DESCRITION  = "[JL-002] Description hasn't been set"
+RULE_SCM	       = "[JL-003] SCM hasn't been set"
+RULE_SCM_TRIGGER = "[JL-004] SCM trigger is polling rather than pulling"
+RULE_SCM_DUPLICATED_TRIGGER = "[JL-005] SCM trigger is duplicated"
+RULE_LABELS 	 = "[JL-006] Restric executions"
+RULE_CLEANUP 	 = "[JL-007] CleanUp Workspace"
+RULE_JAVADOC 	 = "[JL-008] Javadoc"
+RULE_ARTIFACT 	 = "[JL-009] Artifact archiver"
+RULE_HARCODED_SCRIPT = "[JL-010] Harcoded Script over 3 lines"
+RULE_MVN_JOB_TYPE = "[JL-011] Maven Job Type" //http://www.slideshare.net/andrewbayer/seven-habits-of-highly-effective-JL-users-2014-edition
+RULE_SCM_GIT_SHALLOW = "[JL-012] Git shallow"
+RULE_MULTIJOB_TYPE = "[JL-013] Multijob"
 
 // SEVERITY
 HIGH = "High"
@@ -194,4 +184,9 @@ jobs?.findAll{ it.scm && it.scm instanceof hudson.plugins.git.GitSCM && it.scm.g
   		runRule (RULE_SCM_GIT_SHALLOW, it.name, it.description)
     }
   }
+}
+
+jobs?.findAll{ !it.disabled && it instanceof com.tikal.jenkins.plugins.multijob.MultiJobProject && 
+               (!it.getAssignedLabelString() || !it.getAssignedLabelString().contains("multijob")) }.each {
+  runRule (RULE_MULTIJOB_TYPE, it.name, it.description)
 }
