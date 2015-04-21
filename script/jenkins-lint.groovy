8748bd77f350904e9ec83ba3abf2461547e8b4d5
Victor Martinez <VictorMartinezRubio@gmail.com>
2015-04-21 23:24:56 +0200

    Rolling back to previous version since I cannot trap unable to resolve class exception

--- script/jenkins-lint.groovy
+++ script/jenkins-lint.groovy
@@ -172,12 +172,8 @@ jobs?.findAll{ !it.disabled }.each {
   }
   else {
       for(p in it.getPublishersList()) {
-        try {
-          if(p instanceof hudson.plugins.ws_cleanup.WsCleanup) {
-              hasCleanup = true
-          }
-        } catch (org.codehaus.groovy.control.MultipleCompilationErrorsException e){
-          println "WARNING: Workspace Cleanup plugin doesn't exist! Please install it"
+        if(p instanceof hudson.plugins.ws_cleanup.WsCleanup) {
+            hasCleanup = true
         }
         if(p instanceof hudson.tasks.JavadocArchiver && !p.javadocDir) {
             hasJavadoc = false
